f6ea66a9ac8f2a53129beb34f3f93bbe25c351b5
Aashi <45652647+aashi0707@users.noreply.github.com>
2024-03-01 17:33:58 +0530

    Add files via upload

---
+++ NumberIsPositiveOrNegative.java
@@ -0,0 +1,17 @@
+package BASICJAVAPRO;
+
+public class NumberIsPositiveOrNegative {
+
+	public static void main(String[] args) {
+		int a=20;
+		if(a>0)
+		{
+			System.out.println("number is positive");
+		}
+		else
+		{
+			System.out.println("Number is negative");
+		}
+	}
+
+}
