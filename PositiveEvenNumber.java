8d1d3019f46d294b8a2993c16aea7452b5c7d995
Aashi <45652647+aashi0707@users.noreply.github.com>
2024-03-04 22:48:30 +0530

    for loop

---
+++ PositiveEvenNumber.java
@@ -0,0 +1,13 @@
+package BASICJAVAPRO;
+
+public class PositiveEvenNumber {
+
+	public static void main(String[] args) {
+		
+		for(int i=-5;i<=5;i++) {
+			if(i>=0&&i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
