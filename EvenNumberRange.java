3151edaeecad15bc4533472ff4eb9a07c9b593bb
Aashi <45652647+aashi0707@users.noreply.github.com>
2024-03-04 22:30:58 +0530

    while loop

---
+++ EvenNumberRange.java
@@ -0,0 +1,16 @@
+package BASICJAVAPRO;
+
+public class EvenNumberRange {
+
+	public static void main(String[] args) {
+
+	int i=1; //initialization
+	while(i<=10) //condition
+	{
+		if(i%2==0) {
+			System.out.println(i);
+		}
+		 i++; //updation
+	}   
+  }
+}
