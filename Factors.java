69c93e1292b4ea096905a2e505418e5f424eed07
Aashi <45652647+aashi0707@users.noreply.github.com>
2024-03-05 21:14:26 +0530

    n%i==0 number program

---
+++ Factors.java
@@ -0,0 +1,17 @@
+package NumberPrograms;
+
+public class Factors {
+
+	public static void main(String[] args) {
+		int n=8;
+		for(int i=1;i<=n;i++) {
+			if(n%i==0) {
+				System.out.println(i);
+				
+			}
+				
+		}
+		
+	}
+
+}
